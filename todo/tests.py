1d8a5c1082a8a39e0756aad5956bb5be83697cf1
1F10250090 <s1F102500900@iniad.org>
2026-06-25 11:53:56 +0900

    views.py のFlake8警告を修正

--- todo/tests.py
+++ todo/tests.py
@@ -1,13 +1,19 @@
-from django.test import TestCase, Client
-from django.utils import timezone
 from datetime import datetime
+from django.test import Client, TestCase
+from django.utils import timezone
 from todo.models import Task
+
 # Create your tests here.
+
+
 class SampleTestCase(TestCase):
+
     def test_sample1(self):
         self.assertEqual(1 + 2, 3)
 
+
 class TaskModelTestCase(TestCase):
+
     def test_create_task1(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
         task = Task(title='task1', due_at=due)
@@ -17,7 +23,7 @@ class TaskModelTestCase(TestCase):
         self.assertEqual(task.title, 'task1')
         self.assertFalse(task.completed)
         self.assertEqual(task.due_at, due)
-        
+
     def test_is_overdue_future(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
         current = timezone.make_aware(datetime(2024, 6, 30, 0, 0, 0))
@@ -25,12 +31,12 @@ class TaskModelTestCase(TestCase):
         task.save()
 
         self.assertFalse(task.is_overdue(current))
-        
+
     def test_is_overdue_past(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
         current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
         task = Task(title='task1', due_at=due)
-        task.save()  
+        task.save()
 
         self.assertTrue(task.is_overdue(current))
 
@@ -43,13 +49,15 @@ class TaskModelTestCase(TestCase):
     def test_create_task2(self):
         task = Task(title='task2')
         task.save()
-        
+
         task = Task.objects.get(pk=task.pk)
         self.assertEqual(task.title, 'task2')
         self.assertFalse(task.completed)
         self.assertEqual(task.due_at, None)
 
+
 class TodoViewTestCase(TestCase):
+
     def test_index_get(self):
         client = Client()
         response = client.get('/')
